00c1ba64aab58a1f1f4c2564dc8f35c68971e761
Justin D. Harris <>
2021-05-08 09:24:11 -0400

    Minor corrections and clarifications.

--- app/scripts/components/AppTheme.tsx
+++ app/scripts/components/AppTheme.tsx
@@ -1,5 +1,4 @@
-import { ThemeOptions } from '@material-ui/core'
-import { PaletteType, ThemeProvider } from '@material-ui/core'
+import { PaletteType, ThemeOptions, ThemeProvider } from '@material-ui/core'
 import blue from '@material-ui/core/colors/blue'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { createMuiTheme } from '@material-ui/core/styles'
@@ -62,8 +61,8 @@ export class AppTheme extends React.Component<Props, {
 				main: blue[300],
 			}
 
-			const backgroundColor = '#303030'
-			const color = '#eee'
+			const backgroundColor = DARK_MODE_INPUT_BACKGROUND_COLOR
+			const color = DARK_MODE_INPUT_COLOR
 			themeOptions.props = {}
 			themeOptions.props.MuiTextField = {
 				inputProps: {
--- app/scripts/content_script.ts
+++ app/scripts/content_script.ts
@@ -4,6 +4,15 @@ import { applyDefaults, getRules } from './rules/get_rules'
 getRules().then(rules => {
 	// I tried so many ways to get the rules into the page, this seems like one of the only ways because
 	// you can't access storage directly in the injected script.
+
+	// It would be nice to refresh the rules periodically
+	// but it seems like you would need `browser.tabs.executeScript`
+	// and I couldn't get that to work because `browser.tabs` was undefined
+	// even with the activeTab permission.
+	// Maybe it thinks you need the "tabs" permission too even though Chrome docs imply
+	// that you don't need any extra permissions.
+
+	// Inject the rules into the page.
 	applyDefaults(rules)
 	const rulesScript = document.createElement('script')
 	// Add extra escaping to help with escaping quotes and backslashes properly.
