8c2475a0e0fdd4cb199bbfb9be254b8499d50ccf
Justin D. Harris <>
2021-05-09 13:56:19 -0400

    Correct getting rules.

--- app/scripts/rules/get_rules.ts
+++ app/scripts/rules/get_rules.ts
@@ -2,7 +2,7 @@ import { browser } from 'webextension-polyfill-ts'
 import { DEFAULT_RULES, RulesSettings } from './rules'
 
 export async function getRules(): Promise<RulesSettings> {
-	const [{ localRules }, { syncedRules }] = await Promise.all([
+	const [{ rules: localRules }, { rules: syncedRules }] = await Promise.all([
 		browser.storage.local.get('rules'),
 		browser.storage.sync.get('rules'),
 	])
